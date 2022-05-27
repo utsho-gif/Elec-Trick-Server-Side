69d6c0b49045404b7378f1efd5aa61e8b9901e0e
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-05-27 12:53:09 +0600

    added jwt sign

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,10 +19,35 @@ async function run(){
     try{
         await client.connect();
         const productCollection = client.db("elec-trick").collection("products");
+        const userCollection = client.db("elec-trick").collection("users");
 
         //get all services
         app.get('/product', async (req,res) => {
             const result = await productCollection.find().toArray();
+            res.send(result);
+        })
+
+        //update user for JWT
+        app.put('/user/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email: email};
+            const options = { upsert : true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '6d'
+            })
+            res.send({result, token})
+
+        })
+
+        //load a single product
+        app.get('/purchase/:id', async(req, res) => {
+            const id = req.params.id;
+            const result = await productCollection.findOne({_id: ObjectId(id)});
             res.send(result);
         })
     }
