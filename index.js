3d0d199ac8b955d323267f52413ac97a998f108a
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-05-30 22:46:10 +0600

    product manage api created

--- index.js
+++ index.js
@@ -177,6 +177,14 @@ async function run() {
       const result = await productCollection.insertOne(product);
       res.send(result);
   })
+  //delete product
+  app.delete('/product/:id', verifyJWT, verifyAdmin, async(req,res) => {
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await productCollection.deleteOne(query);
+    res.send(result);
+  })
+  
   } 
   finally {
   }
